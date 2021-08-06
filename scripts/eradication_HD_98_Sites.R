f2af69f26b046e2afc7bfabdfc1314f114fc227b
ricardoi <ralcala@ufl.edu>
2021-08-05 23:50:22 -0700

    all shape files SOD

--- scripts/eradication_HD_98_Sites.R
+++ scripts/eradication_HD_98_Sites.R
@@ -3,15 +3,44 @@ library(ggplot2)
 
 setwd("/Users/ricardoi/Box/OSU/SOD/shapefiles/")
 
-
-aoi_boundary_HARV <- st_read("Pram_HD_eradicationNA1EU1/HD_98_Sites.shp")
-st_geometry_type(aoi_boundary_HARV)
-st_crs(aoi_boundary_HARV)
-st_bbox(aoi_boundary_HARV)
-aoi_boundary_HARV
+#---- HD_98_Sites
+HD_98_Sites <- st_read("Pram_HD_eradicationNA1EU1/HD_98_Sites.shp")
+st_geometry_type(HD_98_Sites)
+st_crs(HD_98_Sites)
+st_bbox(HD_98_Sites)
+HD_98_Sites
 
 
 ggplot() + 
-  geom_sf(data = aoi_boundary_HARV, size = 3, color = "black", fill = "cyan1") + 
+  geom_sf(data = HD_98_Sites, size = 3, color = "black", fill = "cyan1") + 
   ggtitle("AOI Boundary Plot") + 
   coord_sf()
+
+#------ Export_Output.shp
+
+EO <- st_read("Pram_resequencedNA1EU1//Export_Output.shp")
+st_geometry_type(EO)
+st_crs(EO)
+st_bbox(EO)
+EO
+
+ggplot() + 
+  geom_sf(data = EO, size = 3, color = "black", fill = "cyan1") + 
+  ggtitle("AOI Boundary Plot") + 
+  coord_sf()
+
+
+#--- Pram_resequencedNA1EU1.shp
+
+Pram_res <- st_read("Pram_resequencedNA1EU1//Pram_resequencedNA1EU1.shp")
+st_geometry_type(Pram_res)
+st_crs(Pram_res)
+st_bbox(Pram_res)
+Pram_res
+
+ggplot() + 
+  geom_sf(data = Pram_res, size = 3, color = "black", fill = "cyan1") + 
+  ggtitle("AOI Boundary Plot") + 
+  coord_sf()
+
+
